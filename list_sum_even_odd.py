03c02274bb81cd739a053e2bcbd6cdf848c5461f
Sipra-code <f20241167@pilani.bits-pilani.ac.in>
2025-12-29 22:05:35 +0530

    Create list_sum_even_odd.py

---
+++ list_sum_even_odd.py
@@ -0,0 +1,13 @@
+numbers = [12, 7, 5, 20, 33, 8]
+
+even_sum = 0
+odd_sum = 0
+
+for num in numbers:
+    if num % 2 == 0:
+        even_sum += num
+    else:
+        odd_sum += num
+
+print("Sum of even numbers:", even_sum)
+print("Sum of odd numbers:", odd_sum)
