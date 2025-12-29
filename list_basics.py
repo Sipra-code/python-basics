4e219d46c0aab596cb6c5a5b277d8092bd254027
Sipra-code <f20241167@pilani.bits-pilani.ac.in>
2025-12-29 22:03:35 +0530

    Create list_basics.py

---
+++ list_basics.py
@@ -0,0 +1,17 @@
+# Creating a list
+numbers = [10, 20, 30, 40, 50]
+
+# Printing the whole list
+print("Numbers:", numbers)
+
+# Accessing elements
+print("First element:", numbers[0])
+print("Last element:", numbers[-1])
+
+# Length of list
+print("Total elements:", len(numbers))
+
+# Looping through the list
+print("All elements:")
+for num in numbers:
+    print(num)
